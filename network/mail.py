73d4d235632e1fc491b0d1af1f72b30dee573571
HavenYang <jidacun@163.com>
2015-04-23 15:43:27 +0800

    send email with payload message and attachment and subject

--- network/mail.py
+++ network/mail.py
@@ -1,16 +1,32 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-sendfrom = "youdon'tknownwhoim"
+sendfrom = "havenyang@viatech.com.cn"
 sendtolist = ['havenyang@viatech.com.cn']
 
-message = """hello, python email
-this is the first mail sent by python
+message = """
+hello python mail 
+this is the first mail sent by python.
 """
+
+#create a instance with attachment
+msg = MIMEMultipart()
+
+#constructure a attachment
+att1 = MIMEText(open('mail.py','rb').read(), 'base64', 'gb2312')
+att1['Content-Type'] = 'application/octet-stream'
+att1['Content-Disposition'] = 'attachment; filename = "123.txt"'
+msg.attach(att1)
+
+msg.set_payload(message)
+
+msg['subject'] = 'dingdong'
 
 try:
 	smtp = smtplib.SMTP()
 	smtp.connect("mailbj.viatech.com.cn")
-	smtp.sendmail(sendfrom, sendtolist, message)
+	smtp.sendmail(sendfrom, sendtolist, msg.as_string())
 	smtp.close()
 	print 'send email OK'
 	
